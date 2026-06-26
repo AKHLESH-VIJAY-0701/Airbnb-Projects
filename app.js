6280a76bc4b60d088704219cb42c2c4568bb1ff7
Akhlesh <beleakhlesh045@gmail.com>
2026-06-26 10:33:09 +0530

    changesss

--- app.js
+++ app.js
@@ -259,7 +259,7 @@ app.use((req, res, next) => {
 
 // ================= HOME ROUTE =================
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("listing/index");
 });
 
 // ================= SEARCH ROUTE =================
